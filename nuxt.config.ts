6c5fa72fb7206d3d8096f575dd347d81b973eb27
arontier-yskim <yskim@arontier.co>
2022-09-01 13:33:52 +0900

    Chore: Add eslint, prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,21 @@
 import { defineNuxtConfig } from 'nuxt';
+import eslintPlugin from 'vite-plugin-eslint';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({});
+export default defineNuxtConfig({
+  srcDir: 'src',
+  typescript: {
+    tsConfig: {
+      compilerOptions: {
+        strict: true,
+        jsx: 'preserve',
+        allowJs: true,
+        noImplicitAny: false,
+        types: [],
+      },
+    },
+  },
+  vite: {
+    plugins: [eslintPlugin()],
+  },
+});
