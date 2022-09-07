7dd63ada1edb6f2edc967e301462cd3ad573bef1
arontier-yskim <yskim@arontier.co>
2022-09-07 18:13:05 +0900

    chore: google fonts module 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,7 @@ export default defineNuxtConfig({
     '@intlify/nuxt3',
     '@pinia/nuxt',
     './src/modules/generate-locale-routes',
+    '@nuxtjs/google-fonts',
   ],
   intlify: {
     localeDir: 'locales',
@@ -17,6 +18,14 @@ export default defineNuxtConfig({
       availableLocales,
       sync: true,
     },
+  },
+  googleFonts: {
+    families: {
+      Raleway: [100, 300, 400, 500, 700, 900],
+      Montserrat: [100, 300, 400, 500, 700, 900],
+    },
+    display: 'swap',
+    download: true,
   },
   css: ['vuetify/lib/styles/main.sass'],
   build: {
